a8eb40c02fc9a9270a1f7f621a8d18ff23bf7d93
sravan <sravan>
2025-01-03 16:48:56 -0500

    selection sort

---
+++ selection_sort.py
@@ -0,0 +1,23 @@
+array = [2,6,3,8,4,1,9,5,7]
+current_small = array[0]
+i = 1
+j = 0
+index = 0
+while j < len(array):
+    current_small = array[j]
+    while i < len(array):
+        if array[i] < current_small:
+            current_small = array[i]
+            index = i
+        i = i+1
+    temp = array[j]
+    array[j] = current_small
+    array[index] = temp
+    j = j+1
+    i = j+1
+    
+print(array)
+
+
+    
+    
